968cf622457e6d4006a031d57cac922fc77e9931
shriwa <shriwa3429@gmail.com>
2024-10-07 08:26:50 +0530

    Build Update for Production

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import copy from "rollup-plugin-copy";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/",
-  build: {
-    outDir: "build",
-  },
+  plugins: [
+    copy({
+      targets: [
+        { src: "public/_redirects", dest: "build" }, // Copy _redirects to dist
+      ],
+    }),
+  ],
 });
